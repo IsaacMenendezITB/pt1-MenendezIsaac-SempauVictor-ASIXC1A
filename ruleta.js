3a4147175c643a75d1c204c6f513213fb31fc527
Victor Sempau <victor.sempau.7e6@itb.cat>
2023-05-09 07:30:08 +0000

    Upload New File

---
+++ ruleta.js
@@ -0,0 +1,31 @@
+const botonCargar = document.getElementById('boton-cargar');
+const botonGenerar = document.getElementById('boton-generar');
+const nombreAleatorio = document.getElementById('nombre-aleatorio');
+
+botonCargar.addEventListener('click', () => {
+  fetch('opciones.txt')
+    .then(respuesta => respuesta.text())
+    .then(texto => {
+      const nombres = texto.split(/\r?\n/);
+      botonGenerar.disabled = false;
+      botonCargar.disabled = true;
+      nombreAleatorio.textContent = '';
+      botonGenerar.addEventListener('click', () => {
+        const indiceAleatorio = Math.floor(Math.random() * nombres.length);
+        const nombre = nombres[indiceAleatorio];
+        nombreAleatorio.textContent = `El nombre aleatorio es: ${nombre}`;
+      });
+    })
+    .catch(error => console.error(error));
+});
+function toggleNightMode() {
+    var body = document.querySelector('body');
+    var button = document.querySelector('#night-mode-button');
+    body.classList.toggle('night-mode');
+    if (body.classList.contains('night-mode')) {
+      button.textContent = 'Modo día';
+    } else {
+      button.textContent = 'Modo noche';
+    }
+  }
+  
